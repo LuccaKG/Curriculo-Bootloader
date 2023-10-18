b99f45056a7da905ab129abf3a6e717b3d21c95a
Lucca Gonçalves <luccagoncalves2000@gmail.com>
2023-10-17 23:10:38 -0300

    Concluindo Compressor RLE para BMP.

--- Compressor RLE para BMP/Compressor_RLE_BMP/main.c
+++ Compressor RLE para BMP/Compressor_RLE_BMP/main.c
@@ -22,8 +22,10 @@ argv[2] será "arg2".
 argv[3] será "arg3".
 */
 
+//char** repBmp; // representação do bmp na memoria
+
 int main(int argc, char* argv[]) {
-    FILE* fp;
+    FILE* fp; 
     char arg[] = "C:\\Users\\lucca\\OneDrive\\Documentos\\cv.bmp";
     errno_t err; // variavel que receberá o código de saída do fopen_s após tentativa de abrir o arquivo
     int totalWidth; // calcula o numero de bits necessarios para definir uma linha
@@ -31,7 +33,9 @@ int main(int argc, char* argv[]) {
     int x, y; // definindo coordenadas do plano cartesiano para depois percorrer a imagem
     int byteAtual = 0; // variavel auxiliar para anotar posição do byte analisado
     int bitCounter = 0; // variavel auxiliar para ajudar a percorrer bit a bit
-
+    int bitQty = 0; // variavel auxiliar para contar quantos bits ja foram lidos. Util para contornar/ignorar os bits de padding nas linhas
+    int consecBits = 0; // variavel auxiliar para contar bits repetidos numa sequencia
+    unsigned char currBit = 0; // variavel auxiliar para identificar bit atual. Minha imagem ja começa com bit preto
     /*
     Para permitir uma execução adequada via terminal, vamos configurar as condições para argc e argv
     */
@@ -89,8 +93,20 @@ e não algum outro tipo de valor inteiro.
     O calculo de bytesPerRow é dado por: ceil(totalWidth/32) * 4
     */
     totalWidth = bf.BIHeader.bitsPerPixel * bf.BIHeader.imageWidth;
-    bytesPerRow = ceil(totalWidth / 32) * 4; 
+    bytesPerRow = ceil((double)totalWidth / 32.0) * 4; 
     /*printf("Largura total: %d\n", bytesPerRow);*/
+
+    /*repBmp = (unsigned char**)malloc(bf.BIHeader.imageHeigth * sizeof(unsigned char *));*/ 
+    /*
+    A função malloc está sendo usada para alocar memória dinamicamente. A quantidade de memória sendo alocada é baseada na altura da
+    imagem (bf.BIHeader.imageHeigth). Essencialmente, está-se alocando espaço para bf.BIHeader.imageHeigth ponteiros de unsigned char.
+
+    O que se está criando é a estrutura básica para uma matriz bidimensional (ou um array de arrays). 
+    repBmp aponta para um array de ponteiros, e cada um desses ponteiros pode, eventualmente, apontar para um array de unsigned char que
+    representará uma linha de pixels na imagem.
+    */
+
+
 
     /*
     O fseek() é uma função da biblioteca padrão em C (e também em algumas outras linguagens) usada para mudar a posição do indicador de
@@ -112,13 +128,36 @@ e não algum outro tipo de valor inteiro.
     
     // percorrendo a imagem
     for (y = 0; y < bf.BIHeader.imageHeigth; y++) { // percorrendo toda a altura da imagem
+        //repBmp[y] = (unsigned char*)malloc((totalWidth * sizeof(char)) + 1); // aloca o espaço para cada linha. Soma 1 para alocar o espaço para o \0 que indica final do array
+        //memset(repBmp[y], '\0', (sizeof(char) * totalWidth + 1)); // seta os pixels como 0 inicialmente
+        bitQty = 0; // zera antes de iniciar a analise de cada linha
         for (x = 0; x < bytesPerRow; x++) { // percorrendo toda a largura da imagem
             fread(&byteAtual, 1, 1, fp);
-            for (bitCounter = 7; bitCounter >= 0; bitCounter--) {
-                printf("%d\n", checkBit(byteAtual, bitCounter));
-            }
-        }
+            for (bitCounter = 7; bitCounter >= 0; bitCounter--) { // percorre cada byte da linha
+                /*printf("%d\n", checkBit(byteAtual, bitCounter));*/ 
+                if (bitQty < totalWidth) {
+                    if (checkBit(byteAtual, bitCounter) == currBit) consecBits++;  // identifica se estamos em uma sequencia; se sim, soma 1 no contador
+                    else { // se nao, retorna o contador pra 1 (pois encontrou 1 de outra cor) e atualiza o currBit (bit atual)                       
+                        printf("%d, ", consecBits);
+                        consecBits = 1; 
+                        currBit = checkBit(byteAtual, bitCounter);
+                    }  
+                    bitQty++;   
+                }                 
+            }  
+        }  
     }
+
+    // Checa se ainda há uma sequência pendente após processar todas as linhas
+    if (consecBits > 0) {
+        printf("%d, ", consecBits);
+    }
+
+    //// for para representar a imagem, que foi armazenada no trecho de codigo acima
+    //for (y = bf.BIHeader.imageHeigth - 1; y >= 0; y--) { // como a imagem é armazenada de cima para baixo e da esquerda para direita, precisamos ler
+    //                                                     // pela logica contraria, para que a representação nao fique de ponta cabeça.
+    //    printf("%s\n", repBmp[y]);
+    //}
 
     fclose(fp);  // sempre feche o arquivo quando terminar de usá-lo
 
